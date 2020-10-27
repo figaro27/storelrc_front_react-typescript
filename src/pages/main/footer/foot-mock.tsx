3c9d6fcb1bb9da0ba72d1d3c287d2290f8b209ce
PandaPo23 <pandapo1223@gmail.com>
2020-10-28 00:53:44 +0800

    insert mock-data

--- src/pages/main/footer/foot-mock.tsx
+++ src/pages/main/footer/foot-mock.tsx
@@ -1,19 +1,55 @@
 export const contactInfo = [
-    
+  "74 Chemin de L'Olivet",
+  "06110 Le Cannet",
+  "Tél : 04 93 90 81 41",
+  "Mob : 06 69 14 17 13",
+  "contact@storeled.fr"
 ];
 
 export const storeLinks = [
   {
-    name: '',
-    path: '',
-  }
+    name: 'Lambrequin lumineux',
+    path: '/lambrequin-lumineux',
+  },
+  {
+    name: 'Stores extérieurs',
+    path: '/store',
+  },
+  {
+    name: 'Pergolas',
+    path: '/pergola',
+  },
+  {
+    name: 'Enseignes',
+    path: '/enseigne',
+  },
+  {
+    name: 'Claustras',
+    path: '/claustras',
+  },
+  {
+    name: 'Radiants',
+    path: '/radiants',
+  },
 ];
 
 export const otherLinks = [
   {
-    name: '',
-    path: '',
-  }
+    name: 'Nos références',
+    path: '/nos-references',
+  },
+  {
+    name: 'Qui sommes-nous',
+    path: '/qui-sommes-nous',
+  },
+  {
+    name: 'F.A.Q.',
+    path: '/faq',
+  },
+  {
+    name: 'Contact',
+    path: '/contact',
+  },
 ]
 
 
