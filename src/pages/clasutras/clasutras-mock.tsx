85198591d121dbc6d0093a8cda589d4bcdf0b314
figaro27 <sweetfigaro27@gmail.com>
2020-10-28 00:53:44 +0800

    insert mock-data

--- src/pages/clasutras/clasutras-mock.tsx
+++ src/pages/clasutras/clasutras-mock.tsx
@@ -1,7 +1,12 @@
 export const clasutrasData = {
-    title: '',
-    subtitle: '',
-    description: '',
-    cover_image: '',
-    slides: []
+    title: 'Claustras',
+    subtitle: 'Concevez votre terrasse idéale',
+    description: "Idéal pour la création d'une terrasse fermée personnalisée, nous vous proposons une gamme complète de claustras, conçus avec des matériaux de qualité, ainsi qu'un large choix de formes et coloris<br/><br/>Réalisés à partir de modules fixes ou amovibles, les claustras peut être modifiés à souhait. Entièrement modulables, ils peuvent être adaptés à toutes les contraintes d'espaces ou de confidentialité.<br/><br/>Elément incontournable pour délimiter la terrasse fermée d'un restaurant, le claustra est de plus en plus présent dans le paysage urbain.<br/><br/>N'hésitez pas à nous contacter pour toute demande d'information, ou obtenir un devis de la part de l'un de nos conseillers.",
+    cover_image: 'https://static.wixstatic.com/media/e49d9d_60d9a795d61d463b848e54e5e63f2885~mv2.png/v1/fill/w_1903,h_509,al_c,q_90,usm_0.66_1.00_0.01/e49d9d_60d9a795d61d463b848e54e5e63f2885~mv2.webp',
+    slides: [
+        "https://static.wixstatic.com/media/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.png/v1/fill/w_609,h_430,al_c,q_85,usm_0.66_1.00_0.01/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.webp",
+        "https://static.wixstatic.com/media/e49d9d_05ba6b336c7245458536f678a9ed16f1~mv2.png/v1/fill/w_609,h_430,al_c,q_85,usm_0.66_1.00_0.01/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.webp",
+        "https://static.wixstatic.com/media/e49d9d_3e8e9d8c832048a98020267067e840a5~mv2.png/v1/fill/w_609,h_430,al_c,q_85,usm_0.66_1.00_0.01/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.webp",
+        "https://static.wixstatic.com/media/e49d9d_60d9a795d61d463b848e54e5e63f2885~mv2.png/v1/fill/w_609,h_430,al_c,q_85,usm_0.66_1.00_0.01/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.webp",
+    ]
 }
--- src/pages/clasutras/clasutras.tsx
+++ src/pages/clasutras/clasutras.tsx
@@ -4,36 +4,27 @@ import { Container, Row, Col, Button, Carousel } from 'react-bootstrap';
 import { Parallax } from 'components/parallax';
 import { useHistory } from 'react-router-dom';
 
-import './clasutras.scss'
+import './clasutras.scss';
+import { clasutrasData } from './clasutras-mock';
 export const Clasutras = () => {
   const history = useHistory();
-  const title = 'Claustras'
-  const text1 = 'Concevez votre terrasse idéale'
-  const text2 = "Idéal pour la création d'une terrasse fermée personnalisée, nous vous proposons une gamme complète de claustras, conçus avec des matériaux de qualité, ainsi qu'un large choix de formes et coloris<br/><br/>Réalisés à partir de modules fixes ou amovibles, les claustras peut être modifiés à souhait. Entièrement modulables, ils peuvent être adaptés à toutes les contraintes d'espaces ou de confidentialité.<br/><br/>Elément incontournable pour délimiter la terrasse fermée d'un restaurant, le claustra est de plus en plus présent dans le paysage urbain.<br/><br/>N'hésitez pas à nous contacter pour toute demande d'information, ou obtenir un devis de la part de l'un de nos conseillers.";
-  const cover = "https://static.wixstatic.com/media/e49d9d_60d9a795d61d463b848e54e5e63f2885~mv2.png/v1/fill/w_1903,h_509,al_c,q_90,usm_0.66_1.00_0.01/e49d9d_60d9a795d61d463b848e54e5e63f2885~mv2.webp";
-  const slides = [
-    "https://static.wixstatic.com/media/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.png/v1/fill/w_609,h_430,al_c,q_85,usm_0.66_1.00_0.01/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.webp",
-    "https://static.wixstatic.com/media/e49d9d_05ba6b336c7245458536f678a9ed16f1~mv2.png/v1/fill/w_609,h_430,al_c,q_85,usm_0.66_1.00_0.01/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.webp",
-    "https://static.wixstatic.com/media/e49d9d_3e8e9d8c832048a98020267067e840a5~mv2.png/v1/fill/w_609,h_430,al_c,q_85,usm_0.66_1.00_0.01/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.webp",
-    "https://static.wixstatic.com/media/e49d9d_60d9a795d61d463b848e54e5e63f2885~mv2.png/v1/fill/w_609,h_430,al_c,q_85,usm_0.66_1.00_0.01/e49d9d_d4fe0feb3b3f4c63a10598969ea5838e~mv2.webp",
-  ]
   return (
       <div>
-        <Parallax title={ title } image={ cover } />
+        <Parallax title={ clasutrasData.title } image={ clasutrasData.cover_image } />
         <Container>
           <div className="claustras__title-section">
-            <h2>{ title }</h2>
-            <label>{text1}</label>
+            <h2>{ clasutrasData.title }</h2>
+            <label>{clasutrasData.subtitle}</label>
           </div>
           <Row className="claustras__description-section">
             <Col md={6} className="text-center">
-              <label dangerouslySetInnerHTML={{__html: text2}}/>
+              <label dangerouslySetInnerHTML={{__html: clasutrasData.description}}/>
               <Button variant="outline-light" size="lg" className='store-detail-demande-btn' onClick={e=>history.push('/demande-de-devis')}>Demande de devis</Button>
             </Col>
             <Col md={6}>
               <div className="clasturas__carousel-container" >
                 <Carousel controls={false} fade={true} indicators={false}>
-                  {slides.map( image => (
+                  {clasutrasData.slides.map( image => (
                   <Carousel.Item>
                     <img className="d-block w-100" src={image} alt=""/>
                   </Carousel.Item>
