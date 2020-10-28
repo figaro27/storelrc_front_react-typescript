cd91898864b6ec19d7cb6ca951b27ae01e4111d6
figaro27 <sweetfigaro27@gmail.com>
2020-10-29 01:16:10 +0900

    Fixed minor issues

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import WebFont from 'webfontloader';
-WebFont.load({
-  google: {
-    families: ['Poppins Web:400,700', 'sans-serif']
-  }
-});
+
 ReactDOM.render(
 
     <App />,
